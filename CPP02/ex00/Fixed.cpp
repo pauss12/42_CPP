d1492cc962128082ee2ef27be8c4f905974273a3
Paula <paulamendezsv@gmail.com>
2025-08-28 23:00:38 +0200

    colores del CPP02/ex00

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -2,11 +2,38 @@
 
 Fixed::Fixed()
 {
-    std::cout << "Default constructor called" << std::endl;
+    std::cout << PURPLE << "Default constructor called" << RESET << std::endl;
     this->_value = 0;
 }
 
 Fixed::Fixed(const Fixed &orig)
 {
-    std::cout << "Copy constructor called" << std::endl;
+    std::cout << ORANGE << "Copy constructor called " << RESET << std::endl;
+    this->_value = orig.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed& orig)
+{
+    std::cout << CYAN << "Copy assignment operator called " << RESET << std::endl;
+    if (this != &orig)
+    {
+        this->_value = this->getRawBits();
+    }
+    return (*this);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << GREEN << "Destructor called " << RESET << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << BLUE << "getRawBits member function called " << RESET << std::endl;
+    return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
 }
