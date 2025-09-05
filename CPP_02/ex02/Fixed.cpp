de07c0eb6a6e7c90e4a512ba4b7c2aaeb8c82ab3
Paula <paulamendezsv@gmail.com>
2025-09-05 20:52:33 +0200

    empezado el CPP_02/ex02

---
+++ CPP_02/ex02/Fixed.cpp
@@ -0,0 +1,79 @@
+# include "Fixed.hpp"
+
+// CONSTRUCTORES Y DESTRUCTORES -----------------------------------------------
+Fixed::Fixed()
+{
+    std::cout << PURPLE << "Default constructor called" << RESET << std::endl;
+    this->_value = 0;
+}
+
+Fixed::Fixed(int const value)
+{
+    std::cout << YELLOW << "Int constructor called" << RESET << std::endl;
+    this->_value = value << _bits;
+}
+
+Fixed::Fixed(float const value)
+{
+    std::cout << ROSE << "Float constructor called" << RESET << std::endl;
+    this->_value = static_cast<int>(roundf(value * (1 << _bits)));
+}
+
+Fixed::Fixed(const Fixed &orig)
+{
+    std::cout << ORANGE << "Copy constructor called " << RESET << std::endl;
+    *this = orig;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << GREEN << "Destructor called " << RESET << std::endl;
+}
+
+
+// GETTERS Y SETTERS --------------------------------- ----------------------------------
+int Fixed::getRawBits(void) const
+{
+    return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
+
+// FUNCIONES PARA SACAR MÁXIMO / MINIMO -------------------------------------------------------
+Fixed&	Fixed::min(Fixed& numA, Fixed& numB)
+{
+
+}
+
+// OTRAS FUNCIONES -----------------------------------------------------------------------------
+int Fixed::toInt(void) const
+{
+    return (this->_value >> _bits);
+}
+
+float Fixed::toFloat(void) const
+{
+    return static_cast<float>(this->_value) / (1 << this->_bits);
+}
+
+// SOBRECARGA DE OPERADORES -----------------------------------------------------------------
+
+Fixed& Fixed::operator=(const Fixed& orig)
+{
+    if (this != &orig)
+    {
+        std::cout << CYAN << "Copy assignment operator called " << RESET << std::endl;
+        this->_value = orig._value;
+    }
+    return (*this);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
+{
+    os << LIGHT_GREEN << fixed.toFloat() << RESET;
+    return os;
+}
+
---
+++ CPP_02/ex02/Fixed.hpp
@@ -0,0 +1,74 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+# define GREEN "\033[0;32m"
+# define RED "\033[0;31m\033[1m"
+# define ORANGE "\001\033[38;5;208m\002"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define YELLOW  "\x1b[33m"
+# define ROSE    "\x1B[38;2;255;151;203m"
+# define LIGHT_BLUE   "\x1B[38;2;53;149;240m"
+# define LIGHT_GREEN  "\x1B[38;2;17;245;120m"
+# define GRAY    "\x1B[38;2;176;174;174m"
+# define RESET "\033[0m"
+
+class Fixed 
+{
+    private:
+        int _value;
+        static const int _bits = 8;
+
+    public:
+        // CONSTRUCTORES Y DESTRUCTORES 
+        Fixed();
+        Fixed(const int value);
+        Fixed(const float valorFloat);
+        ~Fixed();
+        Fixed(const Fixed &orig);
+
+        // SOBRECARGA DE OPERADORES ---
+        Fixed& operator=(const Fixed& orig);
+        // La sobrecarga del operador "<<" es una funcion global; con el atributo "friend" le estas diciendo 
+        // que esta funcion pueda acceder a los valores privados de la clase
+        friend std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+        bool operator<(const Fixed& fixed);
+		bool operator<=(const Fixed& fixed);
+		bool operator>(const Fixed& fixed);
+		bool operator>=(const Fixed& fixed);
+		bool operator==(const Fixed& fixed);
+		bool operator!=(const Fixed& fixed);
+
+		Fixed operator+(const Fixed&) const;
+		Fixed operator-(const Fixed&) const;
+		Fixed operator/(const Fixed&) const;
+		Fixed operator*(const Fixed&) const;
+
+		Fixed& operator++();
+		Fixed operator++(int);
+		Fixed& operator--();
+		Fixed operator--(int);
+
+        // GETTERS Y SETTERS
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+
+        // FUNCIONES PARA SACAR MAXIMO / MINIMO
+        static Fixed&	min(Fixed& numA, Fixed& numB);
+		static const Fixed&	min(const Fixed& numA, const Fixed& numB);
+		static Fixed&	max(Fixed& numA, Fixed& numB);
+		static const	Fixed&	max(const Fixed& numA, const Fixed& numB);
+
+        // OTRAS FUNCIONES 
+        int toInt(void) const;
+        float toFloat(void) const;
+};
+
+#endif
+
