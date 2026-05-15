7c5dadd2584ecd93caf9a8019a5613e09f267cf7
pmendez- <paulamendezsv@gmail.com>
2026-05-15 20:14:09 +0200

    CPP 07, ejercicio 1 hecho

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -7,6 +7,20 @@ void add_and_print(int &n)
 	std::cout << ROSE << "El Valor despues del cambio es ==> " << n << RESET << std::endl;
 }
 
+void up_and_print(std::string &str)
+{
+	std::cout << std::endl << CYAN << "El Valor antes del cambio es ==> " << str << RESET << std::endl;
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (std::islower(str[i]))
+			str[i] = std::toupper(str[i]);
+		else if (std::isupper(str[i]))
+			str[i] = std::tolower(str[i]);
+		
+	}
+	std::cout << ROSE << "El Valor despues del cambio es ==> " << str << RESET << std::endl;
+}
+
 int main(void)
 {
 	{
@@ -17,7 +31,7 @@ int main(void)
 	// Otro test
 	{
 		std::string tab[] = {"Hola", "Mundo", "Desde", "CPP"};
-		iter(tab, 4, add_and_print);
+		iter(tab, 4, up_and_print);
 	}
 
 	return (0);
